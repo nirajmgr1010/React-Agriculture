92f88882fa7241065570742a49a3882a0ace145d
nirajmgr1010 <neerazmgr9@gmail.com>
2026-02-23 22:14:29 +0545

    Added Components, pages and updated App.jsx

---
+++ src/data/product.jsx
@@ -0,0 +1,18 @@
+const product = [
+  {
+    id: 1,
+    name: "Tomato Seeds",
+    category: "Seeds",
+    price: 150,
+    image: "/images/tomato.jpg"
+  },
+  {
+    id: 2,
+    name: "Organic Fertilizer",
+    category: "Fertilizers",
+    price: 500,
+    image: "/images/fertilizer.jpg"
+  }
+];
+
+export default product;
