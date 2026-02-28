61c9c2d834cd285b2857bfabe89b5910a8e74633
nirajmgr1010 <neerazmgr9@gmail.com>
2026-02-28 21:53:16 +0545

    Making structure of folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Shop from "./Pages/Shop";
+import Navbar from "./Components/Navbar";
 import "./App.css";
 
 function App() {
   return (
+    <>
+      <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
     </Routes>
+    </>
   );
 }
 export default App;
